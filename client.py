4658cc15e1a65c679ec075bf0211641d50902ba1
yanis <yanis.louvel402@gmail.com>
2022-12-03 21:32:53 +0100

    Update from French to English

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host, port = socket.gethostbyname(socket.gethostname()), 9001
 client_socket.connect((host, port))
-nom = input("Quelle est votre nom ? ")
+name = input("What is your name ? ")
 
 while True:
 
-	message = input(f"{nom} > ")
-	client_socket.send(f"{nom} > {message}".encode("utf-8"))
+	message = input(f"{name} > ")
+	client_socket.send(f"{name} > {message}".encode("utf-8"))
